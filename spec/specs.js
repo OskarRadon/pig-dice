0f6fd709294bc737cfd7b5b3846373035980ec8d
Michael Schennum and Oskar Radon <mikeschenphoto@gmail.com>
2016-01-27 08:42:47 -0800

    add second failing spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,7 @@
 describe('Player', function() {
-  it("makes a new player", function() {
-    var testPlayer = new Player("Mike");
+  it("makes a new player with currrentRoll array", function() {
+    var testPlayer = new Player("Mike",6);
     expect(testPlayer.playerName).to.equal("Mike");
+    expect(testPlayer.currentRoll).to.eql([6]);
   });
 });
